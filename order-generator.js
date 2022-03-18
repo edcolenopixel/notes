dbb54c225a7b215ec64f3ccfe1ecf8ca5a2afbc0
Paul Nguyen <pnguyen0222@gmail.com>
2022-03-18 07:42:50 -0500

    Update order-generator.js

--- order-generator.js
+++ order-generator.js
@@ -462,7 +462,7 @@ function copypasta(item) {
 	copyText.value = item
 	console.log(copyText.value);
  	navigator.clipboard.writeText(item);
-	  swal({
+	  function a({
 	    title: "Copied!",
 	    text: item,
 	    timer: 2000,
