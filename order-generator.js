b1f8f07949cd7aaa277d38f79a49f257376cf48c
Paul Nguyen <pnguyen0222@gmail.com>
2022-03-18 04:07:18 -0500

    Update order-generator.js

--- order-generator.js
+++ order-generator.js
@@ -439,9 +439,10 @@ function toggleCombos() {
 				tr = `</tr><tr>`;
 				count = 0;
 			}
-//			buffer += `<td><button id="${item}-SELECTED}" name="${item}-SELECTED" value="${item}-SELECTED" onclick=copypasta(` + Menu[item].copypasta + `)>` + ${item} + `</button></td>${tr}`;
-			buffer += `<td><input type="checkbox" id="${item}-SELECTED" name="${item}-SELECTED" value="${item}-SELECTED" ${checked}/>` +
+			buffer += `<td><button id="${item}-SELECTED}" name="${item}-SELECTED" value="${item}-SELECTED" onclick=copypasta(` + Menu[item].copypasta + `)>` + ${item} + `</button></td>${tr}`;
+/*			buffer += `<td><input type="checkbox" id="${item}-SELECTED" name="${item}-SELECTED" value="${item}-SELECTED" ${checked}/>` +
 				`<label for="${item}-SELECTED">${Menu[item].emoji} ${item}</label></td>${tr}`;
+				*/
 		});
 		for (let i = count; i < Settings.COMBO_TABLE_WIDTH; i++) {
 			buffer += `<td></td>`;
