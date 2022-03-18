fd6620d74154a8bc56dbd89a88941aeb98a73e85
Paul Nguyen <pnguyen0222@gmail.com>
2022-03-18 05:17:30 -0500

    Update order-generator.js

--- order-generator.js
+++ order-generator.js
@@ -439,7 +439,7 @@ function toggleCombos() {
 				tr = `</tr><tr>`;
 				count = 0;
 			}
-//			console.log( `<td><button id="${item}-SELECTED" name="${item}-SELECTED" onclick=copypasta(` + Menu[item].copypasta + `)>` + ${item} + `</button></td>${tr}`);
+//			console.log( `<td><button id="${item}-SELECTED" name="${item}-SELECTED" onclick=copypasta(\"` + Menu[item].copypasta + `\")>` + ${item} + `</button></td>${tr}`);
 			buffer += `<td><input type="checkbox" id="${item}-SELECTED" name="${item}-SELECTED" onclick=copypasta(` + item + `) value="${item}-SELECTED" ${checked}/>` +
 				`<label for="${item}-SELECTED">${Menu[item].emoji} ${item}</label></td>${tr}`;
 				
